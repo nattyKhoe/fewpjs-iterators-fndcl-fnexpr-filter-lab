3b6ad8f8d2dd5e83f3140c904f98beee16e3fd58
nattyKhoe <natasha.kusuma@outlook.com>
2022-06-04 23:56:49 +1000

    commit tree a49722c78ba69195ac5cf0962fe4254261514a3b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching (array, name){
+    return array.filter(n=>n.toLowerCase()===name.toLowerCase());
+}
+function fuzzyMatch (array, name){
+    let letters = [];
+    for (let char in name){
+        letters.push(name.charAt(char));
+    };
+    return array.filter(n=>{
+        let nameChar =[];
+        for (let char in n){
+            nameChar.push(n.charAt(char));
+        }
+        for (let i in letters){
+        return letters[i]==nameChar[i]};
+    })
+}
+function matchName (array, string){
+    return array.filter(n=>n.name==string);
+}
